a79cc9ac58a837ddffa14664b802118e3f9e5728
chelsieng <chelsiengmk@gmail.com>
2020-01-28 00:51:56 -0500

    Option 1 & 2 done

--- Assignment 1/Appliance.java
+++ Assignment 1/Appliance.java
@@ -76,7 +76,7 @@ public class Appliance {
 
     //toString Method
     public String toString() {
-        return "Type: " + type + "\nBrand: " + brand + "\nSerial Number: " + serialNumber + "\nPrice: " + price;
+        return "Appliance Serial # " + serialNumber + "\nBrand: " + brand + "\nType: " + type + "\nPrice: $" + price + "0";
     }
 
     //Counting the number of appliances
@@ -94,12 +94,8 @@ public class Appliance {
         return (brand.equals(a1.brand) && type.equals(a1.type) && price == a1.price);
     }
 
-
-    public static void main(String[] args) {
-        System.out.println("Please enter the maximum number of appliances: ");
-        Scanner userInput = new Scanner(System.in);
-        int maxAppliances = userInput.nextInt();
-        int[] inventory = new int[maxAppliances];
+    //Display main menu
+    public static void showMenu() {
         System.out.println("What do you want to do?\n\t1.\tEnter new appliances (password required)");
         System.out.println("\t2.\tChange information of an appliance (password required)");
         System.out.println("\t3.\tDisplay all appliances by a specific brand");
@@ -109,5 +105,192 @@ public class Appliance {
 
     }
 
+    public static void main(String[] args) {
+        int passwordCount = 0;
+        int choice = 0;
+        int applianceCount = 0;
+        int spaceCount = 0;
+        int addCount = 0;
+        int serialNumLocation = 0;
+        boolean serialNumExists = false;
+        long serialNum = 0;
+        String password = null;
+        String brand = null;
+        String type = null;
+        double price = 0;
+        System.out.println("Please enter the maximum number of appliances: ");
+        Scanner userInput = new Scanner(System.in);
+        int maxAppliances = userInput.nextInt();
+        Appliance[] inventory = new Appliance[maxAppliances];
+//            Testing
+//        inventory[0] = new Appliance();
+//        inventory[1] = new Appliance();
+//        inventory[2] = new Appliance();
+        showMenu();
+        choice = userInput.nextInt();
+        while (!(choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5)) {
+            showMenu();
+            choice = userInput.nextInt();
+        }
+        while (choice == 1) {
+            System.out.println("Please enter your password (3 attempts only)");
+            password = userInput.next();
+            while (!(password.equals("c249")) || passwordCount >= 3) {
+                passwordCount++;
+                System.out.println("The password you've entered is incorrect.");
+                password = userInput.next();
+                if ((passwordCount == 2 || passwordCount == 4 || passwordCount == 6)) {
+                    System.out.println("This was your last attempt.");
+                    showMenu();
+                    choice = userInput.nextInt();
+                }
+                while (!(choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5)) {
+                    showMenu();
+                    choice = userInput.nextInt();
+                }
+                if ((passwordCount == 2 || passwordCount == 4 || passwordCount == 6)) {
+                    break;
+                }
+                if (passwordCount == 8) {
+                    System.out.println("Program detected suspicious activities and will terminate immediately!");
+                    System.exit(0);
+                }
+            }
+            while (password.equals("c249")) {
+                passwordCount = 0;
+                System.out.println("How many appliances do you want to enter?");
+                applianceCount = userInput.nextInt();
+                if (applianceCount <= maxAppliances) {
+                    for (int i = 0; i < inventory.length; i++) {
+                        if (inventory[i] == null) {
+                            spaceCount++;
+                        } else {
+                            addCount++;
+                        }
+                    }
+                    if (spaceCount >= applianceCount) {
+                        for (int i = 0; i < inventory.length; i++) {
+                            inventory[i] = new Appliance();
+                        }
+                        System.out.println(applianceCount + " appliances have been added to inventory.");
 
+                    } else {
+                        System.out.println("There is only " + spaceCount + " remaining places available in the inventory.");
+                    }
+                } else {
+                    System.out.println("The inventory does not support this amount of appliances.");
+
+
+                }
+                break;
+            }
+            //to write if statement to break if inventory has been added here
+
+        }
+
+        while (choice == 2) {
+            passwordCount = 0;
+            System.out.println("Please enter your password (3 attempts only)");
+            password = userInput.next();
+            while (!(password.equals("c249"))) {
+                passwordCount++;
+                System.out.println("The password you've entered is incorrect.");
+                password = userInput.next();
+                if ((passwordCount >= 2)) {
+                    System.out.println("This was your last attempt.");
+                    showMenu();
+                    passwordCount = 0;
+                    choice = userInput.nextInt();
+                }
+                while (!(choice == 1 || choice == 2 || choice == 3 || choice == 4 || choice == 5)) {
+                    showMenu();
+                    choice = userInput.nextInt();
+                }
+                if ((passwordCount % 2 == 0)) {
+                    break;
+                }
+            }
+
+            while ((password.equals("c249"))) {
+                System.out.println("Please enter the serial number of the appliance you wish to update");
+                serialNum = userInput.nextLong();
+                for (int i = 0; i < inventory.length; i++) {
+                    inventory[i].getSerialNumber();
+                    if (inventory[i].getSerialNumber() == serialNum) {
+                        serialNumExists = true;
+                        serialNumLocation = i;
+                        break;
+                    } else {
+                        serialNumExists = false;
+                    }
+                }
+                if (serialNumExists == false) {
+                    System.out.println("Oops! There is no appliance with that serial number.\n\t1.Please re-enter another serial number\n\t2.Quit this operation and go back to the main menu");
+                    choice = userInput.nextInt();
+                    while (!(choice == 1 || choice == 2)) {
+                        System.out.println("Oops! There is no appliance with that serial number.\n\t1.Please re-enter another serial number\n\t2.Quit this operation and go back to the main menu");
+                        choice = userInput.nextInt();
+                    }
+                    while (choice == 1) {
+                        System.out.println("Please re-enter >");
+                        serialNum = userInput.nextLong();
+                        break;
+                    }
+                    while (choice == 2) {
+                        showMenu();
+                        break;
+                    }
+                }
+
+                while (serialNumExists == true) {
+                    System.out.println(inventory[serialNumLocation].toString());
+                    System.out.println("What information would you like to change?\n\t1. brand\n\t2. type\n\t3. price\n\t4.Quit\nEnter your choice >");
+                    choice = userInput.nextInt();
+                    while (choice == 1) {
+                        System.out.println("Please enter the new information");
+                        brand = userInput.next();
+                        inventory[serialNumLocation].setBrand(brand);
+                        break;
+                    }
+                    while (choice == 2) {
+                        System.out.println("Please enter the new information");
+                        type = userInput.next();
+                        while (type.equalsIgnoreCase("Fridge") || type.equalsIgnoreCase("Air Conditioner") || type.equalsIgnoreCase("Washer") || type.equalsIgnoreCase("Dryer") || type.equalsIgnoreCase("Freezer") || type.equalsIgnoreCase("Stove") || type.equalsIgnoreCase("Dishwasher") || type.equalsIgnoreCase("Water Heaters") || type.equalsIgnoreCase("Microwave")) {
+                            inventory[serialNumLocation].setType(type);
+                            break;
+                        }
+                        break;
+                    }
+                    while (choice == 3) {
+                        System.out.println("Please enter the new information");
+                        price = userInput.nextDouble();
+                        inventory[serialNumLocation].setPrice(price);
+                        break;
+                    }
+                    while (choice == 4) {
+                        break;
+                    }
+                    if (choice == 4) {
+                        break;
+                    }
+                }
+                if (choice == 4) {
+                    break;
+                }
+            }
+        }
+
+
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
