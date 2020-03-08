a931c8285f1d4ced5cfb9092955ce5102422598a
chelsieng <chelsiengmk@gmail.com>
2020-03-08 00:51:18 -0500

    Welcome message displayed

---
+++ Assignment 3/BibilographyFactory.java
@@ -0,0 +1,8 @@
+public class BibilographyFactory {
+    public static void main(String[] args) {
+        //Displaying welcome message
+        System.out.println("=========================================");
+        System.out.println("WELCOME TO CHELSIE'S BIBILOGRAPHY FACTORY");
+        System.out.println("=========================================");
+    }
+}
