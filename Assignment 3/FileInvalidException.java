d39799e77525de7811c3f32ee10b51b9363eb4c9
chelsieng <chelsiengmk@gmail.com>
2020-03-08 00:40:42 -0500

    javadoc comments done

--- Assignment 3/FileInvalidException.java
+++ Assignment 3/FileInvalidException.java
@@ -1,8 +1,19 @@
+/**
+ * @author Chelsie Ng - 40071692
+ * FileInvalidException Class which throws exception when file is invalid
+ */
 public class FileInvalidException extends Exception {
+    /**
+     * This is a default constructor
+     */
     public FileInvalidException() {
         super("Error: Input file cannot be parsed due to missing information (i.e. month={}, title={}, etc.)");
     }
 
+    /**
+     * This is a constructor to pass any different error message
+     * @param message error message
+     */
     public FileInvalidException(String message) {
         super(message);
     }
