b5038b94f16b9eab702c1dcaf3016cacedb00436
chelsieng <chelsiengmk@gmail.com>
2020-03-18 20:49:29 -0400

    code restructured

--- Assignment 3/BibilographyFactory.java
+++ Assignment 3/BibilographyFactory.java
@@ -11,7 +11,7 @@ public class BibilographyFactory {
         System.out.println("WELCOME TO CHELSIE'S BIBILOGRAPHY FACTORY");
         System.out.println("=========================================");
         //Opening 10 input files
-        for (int i = 1; i < 12; i++) {
+        for (int i = 1; i < 11; i++) {
             File outputFiles = new File("Assignment 3/Output Files");
             File inputFile = new File("Assignment 3/Input Files/Latex" + i + ".bib");
             File IEEFile = new File("Assignment 3/Output Files/IEEE" + i + ".json");
@@ -21,44 +21,42 @@ public class BibilographyFactory {
             PrintWriter pwIEEE = null;
             PrintWriter pwACM = null;
             PrintWriter pwNJ = null;
+            //Opening input file
             try {
-                inputStream = new Scanner(inputFile);//Opening input file
-                pwIEEE = new PrintWriter(new FileOutputStream(IEEFile)); //opening or creating IEEE.json file
-                pwACM = new PrintWriter(new FileOutputStream(ACMFile)); //opening or creating ACM.json file
-                pwNJ = new PrintWriter(new FileOutputStream(NJFile)); //opening or creating NJ.json file
-            } catch (FileNotFoundException e) {
-                if (!inputFile.exists()) {
-                    System.out.println("Could not open input file Latex" + i + ".bib for reading. \n\nPlease check if file exists! Program will terminate after closing any opened files.");
-                    System.exit(0);
-                }
-                // when IEEE file cannot be created
-                if (IEEFile.exists()) {
-                    System.out.println("IEEE" + i + ".json file could not be opened/created.");
-                    outputFiles.delete(); //Deleting directory
-                    inputStream.close();
-                    System.exit(0);
-                }
-                // when ACM file cannot be created
-                if (ACMFile.exists()) {
-                    System.out.println("ACM" + i + ".json file could not be opened/created.");
-                    outputFiles.delete(); //Deleting directory
-                    inputStream.close();
-                    System.exit(0);
-                }
-                // when NJ file cannot be created
-                if (NJFile.exists()) {
-                    System.out.println("NJ" + i + ".json file could not be opened/created.");
-                    outputFiles.delete(); //Deleting directory
-                    inputStream.close();
-                    System.exit(0);
-                }
-            } finally {
+                inputStream = new Scanner(inputFile);
                 inputStream.close();
-                pwIEEE.close();
-                pwACM.close();
-                pwNJ.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("Could not open input file Latex" + i + ".bib for reading. \n\nPlease check if file exists! Program will terminate after closing any opened files.");
+                System.exit(0);
             }
-
+            //Opening or creating IEEE.json file
+            try {
+                pwIEEE = new PrintWriter(new FileOutputStream(IEEFile));
+            } catch (FileNotFoundException E) { // when IEEE file cannot be created
+                System.out.println("IEEE" + i + ".json file could not be opened/created.");
+                outputFiles.delete(); //Deleting directory
+                inputStream.close();
+                System.exit(0);
+            }
+            //Opening or creating ACM.json file
+            try {
+                pwACM = new PrintWriter(new FileOutputStream(ACMFile));
+            } catch (FileNotFoundException e) { // when ACM file cannot be created
+                System.out.println("ACM" + i + ".json file could not be opened/created.");
+                outputFiles.delete(); //Deleting directory
+                inputStream.close();
+                System.exit(0);
+            }
+            //Opening or creating NJ.json file
+            try {
+                pwNJ = new PrintWriter(new FileOutputStream(NJFile));
+            } catch (FileNotFoundException e) { // when NJ file cannot be created
+                System.out.println("NJ" + i + ".json file could not be opened/created.");
+                outputFiles.delete(); //Deleting directory
+                inputStream.close();
+                System.exit(0);
+            }
         }
     }
 }
+
