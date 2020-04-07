72c09fed38e5517c3bda1c2d01fb5bc074af7625
chelsieng <chelsiengmk@gmail.com>
2020-04-07 15:30:06 -0400

    removed single character word

--- Assignment 4/SubDictionaryCreator.java
+++ Assignment 4/SubDictionaryCreator.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SubDictionaryCreator {
     public static void main(String[] args) {
         String word = "";
+        int characterCode = 0;
         try {
             Scanner reader = new Scanner(new FileInputStream("Assignment 4/PersonOfTheCentury.txt"));
             // Reading input file word by word
@@ -22,9 +23,12 @@ public class SubDictionaryCreator {
                 if (word.contains("0") || word.contains("1") || word.contains("2") || word.contains("3") || word.contains("4") || word.contains("5") || word.contains("6") || word.contains("7") || word.contains("8") || word.contains("9")) {
                     continue;
                 }
+                // Skipping single characters except 'a', 'i', 'A' or 'I'
+                if (word.length() == 1 && !((word.equals("a")) || word.equals("A") || word.equals("i") || word.equals("I"))) {
+                    continue;
+                }
                 System.out.println(word);
             }
-
         } catch (FileNotFoundException e) {
             System.out.println("Error! File could not be opened/created! Program will terminate now!");
             System.exit(-1);
